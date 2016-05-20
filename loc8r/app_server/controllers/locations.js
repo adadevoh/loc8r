c81a9c3a49489725328a9212a4c654073cf49118
Joshua Adadevoh <tsc.joshua@gmail.com>
2016-05-20 15:36:04 -0400

    moved data into controllers, and added  few more dependencies: mongodb and mongoose

--- loc8r/app_server/controllers/locations.js
+++ loc8r/app_server/controllers/locations.js
@@ -12,6 +12,7 @@ exports.homelist = function (req, res) {
             title: 'Loc8r',
             strapline: 'Find places with wiFi near you!'
         },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
         locations: [
             {
                 name: 'Starcups',
@@ -43,7 +44,35 @@ exports.homelist = function (req, res) {
 //get location info page
 //Locations Info
 exports.locationInfo = function (req, res) {
-    res.render('location-info', { title: 'Location Info' });
+    res.render('location-info', {
+        title: 'Location Info',
+        name: 'Starcups',
+        rating: 3,
+        address: "125 High Street, Reading, RG6 1PS",
+        hours : {
+            title: "Opening hours",
+            times: ["Monday - Friday : 7:00am - 7:00pm", "Saturday : 8:00am - 5:00pm", "Sunday : closed"]
+        },
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        map : {
+            title: "Location map",
+            image: "http://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x350&sensor=false&markers=51.455041,-0.9690884&scale=2"
+        },
+        CustomerReviews: [
+            {
+                author : 'Simon Holmes',
+                rating: 5,
+                date: Date.now(),
+                comment: "What a great place. I can't say enough good things about it."
+            },
+            {
+                author: "Charlie Chaplin",
+                rating: 3,
+                date: Date.now(),
+                comment: "It was okay. Coffee wasn't great, but the wifi was fast."
+            }
+        ]
+    });
 };
 
 //get 'add review page'
