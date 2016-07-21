7233eda5667fbda288765a30e7d5a1c3b66d6173
Joshua Adadevoh <joshua.adadevoh@citrix.com>
2016-07-21 02:10:46 -0400

    updated app port

--- loc8r/app_server/controllers/locations.js
+++ loc8r/app_server/controllers/locations.js
@@ -2,7 +2,7 @@
 
 var request = require('request');
 var apiOptions = {
-    server : "http://localhost:3000"
+    server : "http://localhost:1337"
 };
 if(process.env.NODE_ENV === 'production'){
     apiOptions.server = "https://";
